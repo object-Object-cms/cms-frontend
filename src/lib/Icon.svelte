f33eb017f92940462d22463a6aa59b31bea35bc9
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-02 00:20:34 +0200

    Fix tooltip padding.

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -18,13 +18,16 @@
 <div class="icon group relative" bind:this={container} on:click>
     <span class="material-icons"><slot /></span>
     {#if tooltip}
-        <span
-            class="pointer-events-none invisible absolute z-50 w-max rounded-sm
-                bg-black bg-opacity-60 px-2 py-1 font-medium text-white opacity-0
+        <div
+            class="pointer-events-none invisible absolute z-50 p-1 opacity-0
                 transition-[opacity,visibility] group-hover:visible group-hover:opacity-100 group-hover:delay-1000"
             bind:this={tooltipElem}
         >
-            {tooltip}
-        </span>
+            <div
+                class="w-max rounded-sm bg-black bg-opacity-60 px-2 py-1 font-medium text-white"
+            >
+                {tooltip}
+            </div>
+        </div>
     {/if}
 </div>
