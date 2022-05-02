707e0b8d2bb068377249646ac411e41c364f133a
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-02 23:40:59 +0200

    Add clickable slide indicators.

--- src/lib/Slider.svelte
+++ src/lib/Slider.svelte
@@ -9,18 +9,18 @@
     export let slides: Slide[];
     export let cycleTime = 3000;
 
-    let slide = 0;
+    let slideIndex = 0;
 
     function clampWithRollover(min, max, value) {
         return value > max ? min : value < min ? max : value;
     }
 
     function prevSlide() {
-        slide = clampWithRollover(0, slides.length - 1, slide - 1);
+        slideIndex = clampWithRollover(0, slides.length - 1, slideIndex - 1);
     }
 
     function nextSlide() {
-        slide = clampWithRollover(0, slides.length - 1, slide + 1);
+        slideIndex = clampWithRollover(0, slides.length - 1, slideIndex + 1);
     }
 
     let cycleInterval: number | undefined;
@@ -34,17 +34,23 @@
         clearInterval(cycleInterval);
     }
 
+    function showSlide(index) {
+        if (index >= 0 && index < slides.length) {
+            slideIndex = index;
+        }
+    }
+
     startCycleTimer();
 </script>
 
 <div
-    class="w-full overflow-hidden"
+    class="relative w-full overflow-hidden"
     on:mouseenter={stopCycleTimer}
     on:mouseleave={startCycleTimer}
 >
     <div
         class="flex w-full items-start transition-transform duration-300"
-        style="transform: translate({-100 * slide}%);"
+        style="transform: translate({-100 * slideIndex}%);"
     >
         {#each slides as slide}
             <div class="relative min-w-full">
@@ -54,7 +60,7 @@
                     alt="{slide.title} slide image"
                 />
                 <div
-                    class="absolute left-1/2 bottom-4 -translate-x-1/2 text-center
+                    class="absolute left-1/2 bottom-8 -translate-x-1/2 text-center
                         {slide.textColor === 'black'
                         ? 'text-black'
                         : 'text-white'}"
@@ -63,6 +69,15 @@
                     <p>{slide.description}</p>
                 </div>
             </div>
+        {/each}
+    </div>
+    <div class="absolute left-1/2 bottom-4 flex -translate-x-1/2 space-x-4">
+        {#each slides as _slide, i}
+            <div
+                class="h-2 w-8 cursor-pointer rounded-sm border-2 border-white bg-black transition-opacity
+                    {slideIndex === i ? 'opacity-75' : 'opacity-20'}"
+                on:click={() => showSlide(i)}
+            />
         {/each}
     </div>
 </div>
