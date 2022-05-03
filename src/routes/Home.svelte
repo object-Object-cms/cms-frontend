3a491aab745defe3bd27c9bdfc1d7b2aa03b1c0f
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-03 21:48:46 +0200

    Add test of DynamicComponent in Home route.

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-    import Slider from "../lib/Slider.svelte";
+    import DynamicComponent from "../lib/DynamicComponent.svelte";
 </script>
 
-<Slider
-    slides={[
-        {
-            title: "A slide",
-            description: "This is a cool slide",
-            imageUrl: "dummydata/a.png",
-            textColor: "white"
-        },
-        {
-            title: "More than one",
-            description: "Yes there can be more than one slide",
-            imageUrl: "dummydata/b.png",
-            textColor: "white"
-        },
-        {
-            title: "Wraps around",
-            description: "After this slide you should see the first one again",
-            imageUrl: "dummydata/c.png",
-            textColor: "white"
-        }
-    ]}
-    cycleTime={5000}
-/>
+{#await fetch("dummydata/test.json").then((resp) => resp.json())}
+    <p>loading</p>
+{:then component}
+    <DynamicComponent descriptor={component} />
+{/await}
