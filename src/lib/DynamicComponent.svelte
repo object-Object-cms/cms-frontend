e41e09d8079667df4767023905092bb82487a070
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-03 21:48:20 +0200

    Implement dynamic components.

---
+++ src/lib/DynamicComponent.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import DynamicComponents from "./DynamicComponents.svelte";
+    import Icon from "./Icon.svelte";
+    import Slider from "./Slider.svelte";
+
+    interface ComponentDescriptor {
+        name: string;
+        props: any;
+        children: ComponentDescriptor[];
+    }
+
+    export let descriptor: ComponentDescriptor;
+
+    const namedComponents = {
+        Icon,
+        Slider
+    };
+</script>
+
+<svelte:component this={namedComponents[descriptor.name]} {...descriptor.props}>
+    <DynamicComponents components={descriptor.children} />
+</svelte:component>
---
+++ src/lib/DynamicComponents.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import DynamicComponent from "./DynamicComponent.svelte";
+
+    export let components: any[];
+</script>
+
+{#each components as component}
+    {#if typeof component === "object"}
+        <DynamicComponent descriptor={component} />
+    {:else}
+        {component}
+    {/if}
+{/each}
