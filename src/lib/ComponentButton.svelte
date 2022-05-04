e297f33e8cfce3206561a419a28f54d181ae10dc
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-04 18:31:43 +0200

    Expose click events of ComponentButton to parent.

--- src/lib/ComponentButton.svelte
+++ src/lib/ComponentButton.svelte
@@ -6,6 +6,7 @@
 <div
     class="flex select-none items-center space-x-4 px-2 py-1 hover:bg-slate-600"
     title={name}
+    on:click
 >
     <span class="material-icons-outlined md-18">{icon}</span>
     <span>{name}</span>
