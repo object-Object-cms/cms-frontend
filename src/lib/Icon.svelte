d46a55b5dfe76d7d32f95629f0b8216683007acb
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-05 21:29:27 +0200

    Allow icon hover tint to be inverted.

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -6,6 +6,7 @@
     let lastPopper: Instance | undefined;
 
     export let tooltip: string | undefined = undefined;
+    export let inverted = false;
 
     $: {
         lastPopper?.destroy();
@@ -15,7 +16,12 @@
     }
 </script>
 
-<div class="icon group relative" bind:this={container} on:click>
+<div
+    class="group relative h-8 w-8 cursor-pointer select-none rounded-full p-1 transition-colors
+        {inverted ? 'hover:bg-white' : 'hover:bg-black'} hover:bg-opacity-20"
+    bind:this={container}
+    on:click
+>
     <span class="material-icons"><slot /></span>
     {#if tooltip}
         <div
