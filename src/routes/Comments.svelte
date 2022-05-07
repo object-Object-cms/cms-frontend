e54462ddf44dfe21fd54c4f3e4203dd398b14a23
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-07 18:52:20 +0200

    Require an account to post comments.

--- src/routes/Comments.svelte
+++ src/routes/Comments.svelte
@@ -13,20 +13,13 @@
         return fetch("dummydata/comments.json").then((resp) => resp.json());
     }
 
-    let author = $currentAccount?.username ?? "";
     let content = "";
 
     function postComment(ev: SubmitEvent) {
         ev.preventDefault();
-        if (author !== "" && content !== "") {
-            console.log("Posting comment:");
-            console.log("author:", author);
-            console.log("content:", content);
-            author = "";
-            content = "";
-        } else {
-            alert("Please enter your name and a comment");
-        }
+        console.log("Posting comment:");
+        console.log("content:", content);
+        content = "";
     }
 </script>
 
@@ -38,31 +31,29 @@
     </div>
 {:then comments}
     <div class="container mx-auto p-2 space-y-2">
-        <form
-            class="bg-gray-300 mx-auto max-w-lg rounded-md p-2 space-y-2"
-            on:submit={postComment}
-        >
-            <label class="block w-full">
-                Name:
-                <input
-                    class="w-full bg-gray-100 p-2 rounded"
-                    type="text"
-                    required
-                    bind:value={author}
-                />
-            </label>
-            <label class="block w-full">
-                Comment:
-                <textarea
-                    class="w-full resize-none bg-gray-100 p-2 rounded"
-                    required
-                    bind:value={content}
-                />
-            </label>
-            <button class="button button-blue" type="submit">
-                Post comment
-            </button>
-        </form>
+        {#if $currentAccount}
+            <form
+                class="bg-gray-300 mx-auto max-w-lg rounded-md p-2 space-y-2"
+                on:submit={postComment}
+            >
+                <label class="block w-full">
+                    Comment:
+                    <textarea
+                        class="w-full resize-none bg-gray-100 p-2 rounded"
+                        required
+                        bind:value={content}
+                    />
+                </label>
+                <button class="button button-blue" type="submit">
+                    Post comment
+                </button>
+            </form>
+        {:else}
+            <p class="text-red-600 text-center">
+                You need to login to post a comment
+            </p>
+        {/if}
+
         {#each comments as comment}
             <div class="bg-slate-300 p-2 rounded">
                 <p class="text-blue-600 font-bold">{comment.author}</p>
