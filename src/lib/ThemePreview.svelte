4c7087043cd995c94f13a5a9edc833cf6b3743a2
awtlkrakow <94865935+awtlkrakow@users.noreply.github.com>
2022-05-29 19:39:01 +0200

    Update theme preview.

--- src/lib/ThemePreview.svelte
+++ src/lib/ThemePreview.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
     import type { ThemeVariables } from "./themes";
     import ThemedComponent from "./ThemedComponent.svelte";
+    import NewsBox from "./NewsBox.svelte";
+    import Paragraph from "./Paragraph.svelte";
 
     export let themeVariables: Partial<ThemeVariables>;
+
+    const markdownPreviewSource = `# Sample text
+
+This is an example text component, styled with **Mark***down*
+
+[A link to nowhere](javascript:void(0))`;
 </script>
 
 <p class="text-xs text-gray-600 group-focus-within:text-blue-600">Preview:</p>
 <ThemedComponent {themeVariables}>
-    <div class="themed-background themed-font border-2 border-gray-600 p-4">
-        <p class="themed-text">Sample text</p>
-        <p class="themed-link cursor-pointer hover:underline">This is a link</p>
+    <div
+        class="themed-background themed-text themed-font border-2 border-gray-600 p-4"
+    >
+        <Paragraph textContent={markdownPreviewSource} />
+        <NewsBox
+            title="Example"
+            description="This is an example of a box and a button"
+            linkText="Click me"
+            linkUrl="javascript:void(0)"
+        />
     </div>
 </ThemedComponent>
